69bebd2edd0dae4d9609f604f7566a52c21be3e6
Ítalo Góes <italogoes85@gmail.com>
2023-06-08 16:30:14 -0300

    .

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+http.listen(3000, () => {
+    console.log('App Rodando!')
+})
+
+io.on('connection', (socket) => {
+
+    socket.on('disconnect', () => {
+        console.log('x desconectou ' + socket.id)
+    })
+
+    socket.on('msg', (data) => {
+        io.emit('showmsg', data)
+        console.log(data)
+    })
+
+})
